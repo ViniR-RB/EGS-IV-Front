d46011f7ebcd9fc15d3308b01a8a497ff73201c5
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2023-03-25 20:10:13 -0300

    Fix: TodoModel

--- lib/app/todo/adapter/todo_adapter.dart
+++ lib/app/todo/adapter/todo_adapter.dart
@@ -2,6 +2,6 @@ import 'package:front/app/todo/model/todo_model.dart';
 
 abstract class TodoAdapter {
   static TodoModel toModel(dynamic data) {
-    return TodoModel(title: data['title'], isCompleted: data['isCompleted']);
+    return TodoModel(title: data['title'], isCompleted: data['completed']);
   }
 }
