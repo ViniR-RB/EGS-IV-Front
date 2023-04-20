6196a66f490cd1df3efe3f6d29b83cde73d4f822
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2023-04-20 19:11:18 -0300

    Configuration CI

--- lib/app/core/configuration/configuration.dart
+++ lib/app/core/configuration/configuration.dart
@@ -1,9 +1,9 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-abstract class ConfigurateEnv {
-  ConfigurateEnv._();
+// abstract class ConfigurateEnv {
+//   ConfigurateEnv._();
 
-  static String get baseUrl => _get('INTEGRACAO_URL');
+//   static String get baseUrl => _get('INTEGRACAO_URL');
 
-  static String _get(String name) => dotenv.env[name] ?? 'Not Found';
-}
+//   static String _get(String name) => dotenv.env[name] ?? 'Not Found';
+// }
